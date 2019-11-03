4180b926db9dbc59b311dd5097d22a03ad970b28
lhyUnited <lhy_1314@yeah.net>
2019-11-03 10:19:48 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import LoginPage from '@/components/LoginPage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginPage
+    }
+  ]
+})
